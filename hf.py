71899ecd0bfed825507e867bcc506e54e2bea1f3
JeffWang <jeffreyw200068@gmail.com>
2024-07-15 11:19:52 +0800

    Add hf.py

---
+++ hf.py
@@ -0,0 +1,15 @@
+from transformers import pipeline
+
+# Load the pre-trained sentiment analysis model
+sentiment_analysis = pipeline(
+"sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+
+input_text = [
+"It’s a great app, there are no problem with this app and has a great user interface. I am very happy with the customer service! This increased our efficiency."
+]
+
+# Perform sentiment analysis on the input text
+result = sentiment_analysis(input_text)
+
+# Print the result
+print(result)
